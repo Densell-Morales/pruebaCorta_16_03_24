1edcbb01805ae17d5c5bffbc9b2085b91b4ce27c
andres <dguevaram4@miumg.edu.gt>
2024-03-16 10:51:35 -0600

    segundo commit

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void bubbleSort(int arr[], int n) {
     }
 }
 int main() {
-
+/*
     int n = 5;
     int arr[n];
     cout << "Ingresas 5 numero a tu eleccion: ";
@@ -22,8 +22,41 @@ int main() {
     cout << "El orden de los numeros ingresados es: ";
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
-    }    return 0;
-}
 
+    }    return 0;
+}*/
+
+    const int n_arreglos = 10;
+    int searchArray[n_arreglos] = {324, 457, 6789, 541345, 7, 6, 8965, 12, 32, 4815};
+
+
+    int Busqueda_array, location;
+
+    while(1) {
+        cout << endl << "Ingresa un numero: " << endl;
+        cin >> Busqueda_array;
+
+
+        if(Busqueda_array == -1) break;
+
+
+        for(int i=0; i < n_arreglos; i++) {
+            if(searchArray[i] == Busqueda_array) {
+                location = i;
+                break;
+            }
+
+        }
+
+
+        if(location != -1) {
+            cout << Busqueda_array << " esta en la posicion [" << location << "] del arreglo." << endl;
+        }
+        else {
+            cout << Busqueda_array << " no existe en el arreglo." << endl;
+        }
+    }
+
+}
 
 
